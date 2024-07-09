13d39e68ced7a781d70d70795faf93ee7641031a
ABHI <abhi202864@gmail.com>
2024-07-09 22:28:22 +0530

    add all country

--- script.js
+++ script.js
@@ -5,14 +5,53 @@ const toCurrencyElement = document.querySelector(".toCurrency");
 const resultElement = document.querySelector(".result");
 
 const countries = [
-    {
-        code: "USD",
-        name: "United States Dollar"
-    },
-    {
-        code: "INR",
-        name: "Indian Ruppe"
-    }
+    { code: "USD", name: "United States Dollar" },
+    { code: "INR", name: "Indian Ruppe" },
+    { code: "KRW", name: "South Korean Won" },
+    { code: "MXN", name: "Mexican Peso" },
+    { code: "MYR", name: "Malaysian Ringgit" },
+    { code: "NOK", name: "Norwegian Krone" },
+    { code: "NZD", name: "New Zealand Dollar" },
+    { code: "PEN", name: "Peruvian Sol" },
+    { code: "PHP", name: "Philippine Peso" },
+    { code: "PLN", name: "Polish Złoty" },
+    { code: "RON", name: "Romanian Leu" },
+    { code: "RUB", name: "Russian Ruble" },
+    { code: "SEK", name: "Swedish Krona" },
+    { code: "SGD", name: "Singapore Dollar" },
+    { code: "THB", name: "Thai Baht" },
+    { code: "TRY", name: "Turkish Lira" },
+    { code: "TWD", name: "Taiwan New Dollar" },
+    { code: "UAH", name: "Ukrainian Hryvnia" },
+    { code: "USD", name: "United States Dollar" },
+    { code: "UYU", name: "Uruguayan Peso" },
+    { code: "VND", name: "Vietnamese Đồng" },
+    { code: "ZAR", name: "South African Rand" },
+    { code: "AUD", name: "Australian Dollar" },
+    { code: "BRL", name: "Brazilian Real" },
+    { code: "CAD", name: "Canadian Dollar" },
+    { code: "CHF", name: "Swiss Franc" },
+    { code: "CLP", name: "Chilean Peso" },
+    { code: "CNY", name: "Chinese Yuan" },
+    { code: "COP", name: "Colombian Peso" },
+    { code: "CZK", name: "Czech Koruna" },
+    { code: "DKK", name: "Danish Krone" },
+    { code: "EGP", name: "Egyptian Pound" },
+    { code: "EUR", name: "Euro" },
+    { code: "GBP", name: "British Pound Sterling" },
+    { code: "HKD", name: "Hong Kong Dollar" },
+    { code: "HRK", name: "Croatian Kuna" },
+    { code: "HUF", name: "Hungarian Forint" },
+    { code: "IDR", name: "Indonesian Rupiah" },
+    { code: "ILS", name: "Israeli New Shekel" },
+    { code: "INR", name: "Indian Rupee" },
+    { code: "ISK", name: "Icelandic Króna" },
+    { code: "JPY", name: "Japanese Yen" },
+    { code: "AED", name: "United Arab Emirates Dirham" },
+    { code: "ARS", name: "Argentine Peso" }
+
+
+
 ];
 
 countries.forEach((country) => {
