fe2a57263457fad3fe333e4d2737b3380bef0d28
ABHI <abhi202864@gmail.com>
2024-08-08 23:02:48 +0530

    login page

--- app.js
+++ app.js
@@ -6,11 +6,14 @@ const ejsMate = require("ejs-mate");
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.engine("ejs", ejsMate);
+app.use(express.static(path.join(__dirname, "public")));
 
 // home route
 app.get("/", (req, res) => {
     res.render("index", { title: "POST-Book" });
 });
+
+
 
 app.listen(8080, () => {
     console.log("Doking Port- 8080");
