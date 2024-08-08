d332f7e281da88817884580519521e36966b17e9
ABHI <abhi202864@gmail.com>
2024-08-08 18:01:18 +0530

    re structure project

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const ejsMate = require("ejs-mate");
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.engine("ejs", ejsMate);
+
+// home route
+app.get("/", (req, res) => {
+    res.render("index", { title: "POST-Book" });
+});
+
+app.listen(8080, () => {
+    console.log("Doking Port- 8080");
+});
