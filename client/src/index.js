c593887d582906d2b9f9e3896a74896828f5fbdd
ABHI <abhi202864@gmail.com>
2024-09-17 22:09:35 +0530

    landing page complete

--- client/src/index.js
+++ client/src/index.js
@@ -3,11 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 
 import HomePage from './landingPage/home/HomePage';
-import Brokerage from './landingPage/pricing/Brokerage';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <HomePage />
-    <Brokerage />
   </React.StrictMode>
 );
--- client/src/landingPage/home/Education.jsx
+++ client/src/landingPage/home/Education.jsx
@@ -2,7 +2,26 @@ import React from "react";
 
 const Education = () => {
   return (
-    <h1>Education</h1>
+    <div className="container mt-5">
+      <div className="row">
+        <div className="col-6">
+          <img src="assets/education.svg" />
+        </div>
+        <div className="col-6 p-5">
+          <h4>Free and open market education</h4>
+          <p className="mt-5">
+            Varsity, the largest online stock market education book in the world
+            covering everything from the basics to advanced trading.
+          </p>
+          <a href="" style={{ textDecoration: "none" }}>Varsity <i class="fa fa-long-arrow-right" aria-hidden="true"></i></a>
+          <p className="mt-5">
+            TradingQ&A, the most active trading and investment community in
+            India for all your market related queries.
+          </p>
+          <a href="" style={{ textDecoration: "none" }}>TradingQ&A <i class="fa fa-long-arrow-right" aria-hidden="true"></i></a>
+        </div>
+      </div>
+    </div>
   )
 }
 
