9e91cbfee765c703e33ae66244b38283885cc785
ABHI <abhi202864@gmail.com>
2024-09-18 22:38:46 +0530

    product page complete

--- client/src/landingPage/products/Hero.jsx
+++ client/src/landingPage/products/Hero.jsx
@@ -1,7 +1,16 @@
 
 const Hero = () => {
   return (
-    <h1>Hero</h1>
+    <div className="container border-bottom">
+      <div className="row p-5 mt-5 mb-5">
+        <div className="text-center mt-5 mb-5">
+          <h1 className="text-muted" style={{ fontSize: "3rem" }}>Technology</h1>
+          <h5 className="text-muted mt-4" style={{ letterSpacing: "1px" }}>Sleek, modern, and intuitive trading platforms</h5>
+          <p className="text-muted mt-3" style={{ fontSize: "1.2rem" }}>Check out our <a href="https://zerodha.com/investments" style={{ textDecoration: "none" }} target="_blank">investment offerings <i class="fa fa-long-arrow-right" aria-hidden="true"></i></a></p>
+        </div>
+
+      </div>
+    </div>
   )
 }
 
--- client/src/landingPage/products/LeftSection.jsx
+++ client/src/landingPage/products/LeftSection.jsx
@@ -1,8 +1,26 @@
 
 
-const LeftSection = () => {
+const LeftSection = ({ imageUrl, productTitel, productDes, tryDemo, learnMore, googlePlay, appStore }) => {
   return (
-    <h1>Left S</h1>
+    <div className="container">
+      <div className="row p-5">
+        <div className="col-6">
+          <img src={imageUrl} />
+        </div>
+        <div className="col-6 p-5 mt-5">
+          <h1 className="text-muted">{productTitel}</h1>
+          <p className="text-muted" style={{ lineHeight: "1.8rem", fontSize: "1.3rem" }}>{productDes}</p>
+          <div className="mt-3">
+            <a href={tryDemo} style={{ fontSize: "1rem", fontWeight: "500", textDecoration: "none" }}>Try demo <i class="fa fa-long-arrow-right" aria-hidden="true"></i></a>
+            <a href={learnMore} style={{ marginLeft: "1rem", fontSize: "1rem", fontWeight: "500", textDecoration: "none" }}>Learn more <i class="fa fa-long-arrow-right" aria-hidden="true"></i></a>
+          </div>
+          <div className="mt-3">
+            <a href={googlePlay}><img src="assets/googlePlayBadge.svg" /></a>
+            <a href={appStore} style={{ marginLeft: "2rem" }}><img src="assets/appstoreBadge.svg" /></a>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
