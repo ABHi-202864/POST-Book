c864626514dc44daaceb006a77a77d65e78f5c14
ABHI <abhi202864@gmail.com>
2024-09-25 22:14:40 +0530

    Create Schema For model

--- server/index.js
+++ server/index.js
@@ -4,11 +4,18 @@ if (process.env.NODE_ENV != "production") {
 
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 3002;
+const url = process.env.MONGO_URL;
+
+
 
 app.get("/", (req, res) => {
   res.send("Hello");
 });
 
-app.listen(3002, () => {
+app.listen(PORT, () => {
   console.log("Doking on port = 3002");
+  mongoose.connect(url);
 }); 
